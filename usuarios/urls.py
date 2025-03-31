0f83bb21719ebafaf284d912247a183904ee8c78
Valente Alexsander <alevtelles@gmail.com>
2025-03-31 16:20:49 -0300

    feat: add login, auth

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,4 +3,5 @@ from usuarios import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"),
+    path('login/', views.login, name="login"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == 'GET':
@@ -30,5 +32,29 @@ def cadastro(request):
             password=senha
         )
 
-
         return redirect('/usuarios/login/')
+    
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/mentorados/')
+        
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
+        return redirect('login')
+
+        
+
+
+
+
+        
+    
